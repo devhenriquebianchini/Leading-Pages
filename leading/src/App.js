cc1512475b1f92969c0ca1437125864bf98cb8a0
devhenriquebianchini <brawlstarscorenelruff@gmail.com>
2024-07-09 00:43:41 -0300

    commit 2

--- leading/src/App.js
+++ leading/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home} />
+        </Routes>
+      </Router>
     </div>
   );
 }
